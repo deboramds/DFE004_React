a3fb561617df15197cd7ad2fd28cbe6b535f6914
deboramds <deborah.mart@hotmail.com>
2023-07-04 19:19:33 -0300

    print-filmes-na-tela

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 
- import React, { useEffect } from 'react';
+ import React, { useEffect, useState } from 'react';
+ import './App.css';
  import Tmdb from './Tmdb';
+ import MovieRow from './components/MovieRow';
+ 
 
  export default () => {
+
+  const[movieList, setMovieList]= useState ([]);
   
    useEffect(()=>{ //carregar o conteúdo
      const loadAll = async () => {
        //Pegando a lista TOTAL
       let list = await Tmdb.getHomeList(); //Await =>espera a requisição da api para carregar o conteúdo
-      console.log(list);
+      setMovieList(list);
      }
     
      loadAll();
    }, []);
 
    return (
-     <div>
-       Olá mundo
+     <div className = "page">
+      <section className="lists">
+        {movieList.map((item, key)=>(
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
      </div>
   );
   }
